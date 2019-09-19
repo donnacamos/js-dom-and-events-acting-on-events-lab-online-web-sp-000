71b270b4f16b0d4135c69d7592a0acab28a52140
Donna Amos <donnacamos.developer@gmail.com>
2019-09-19 18:14:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function retrieveEmployeeInformation(){
   const input = $('form input:first')[0];
   return input.value;
 }
+
+function addNewElementAsLi(){
+  const name = retrieveEmployeeInformation();
+  const ul = $('ul.employee-list')[0];
+  const li = document.createElement('li')
+  li.innerHTML = name;
+  ul.appendChild(li);
+}
+
